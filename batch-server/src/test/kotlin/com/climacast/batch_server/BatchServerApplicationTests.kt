88b37ac61d73a6dd3b4c7a7b072e7cd7b4d4685a
yunhozz <qkrdbsgh1121@naver.com>
2024-10-30 22:17:26 +0900

    [#5] CHORE: Batch Server 모듈 추가

---
+++ batch-server/src/test/kotlin/com/climacast/batch_server/BatchServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.climacast.batch_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
