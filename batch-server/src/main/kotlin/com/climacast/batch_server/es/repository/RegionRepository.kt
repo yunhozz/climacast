1015a2f32af2a1a9f107a53454dc94383ea73617
yunhozz <qkrdbsgh1121@naver.com>
2024-11-01 11:29:32 +0900

    [#5] REMOVE: API 변경에 따른 모델 삭제

--- batch-server/src/main/kotlin/com/climacast/batch_server/es/repository/RegionRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.climacast.batch_server.es.repository
-
-import com.climacast.batch_server.es.document.Region
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
-
-interface RegionRepository: ElasticsearchRepository<Region, String>
