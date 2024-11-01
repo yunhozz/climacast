b159273d99d2e760518826443033c878f9bf9bab
yunhozz <qkrdbsgh1121@naver.com>
2024-11-01 11:25:23 +0900

    [#5] CHORE: WebFlux 의존성 추가

--- batch-server/build.gradle.kts
+++ batch-server/build.gradle.kts
@@ -4,6 +4,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	testImplementation("org.springframework.batch:spring-batch-test")
+	testImplementation("io.projectreactor:reactor-test")
 	runtimeOnly("com.mysql:mysql-connector-j")
 }
