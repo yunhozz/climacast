fde03f4562b9aa45d95facf92bf102d2b1cb98e4
yunhozz <qkrdbsgh1121@naver.com>
2024-11-12 20:28:47 +0900

    [#5] FIX: 스프링 빈 주입 오류 수정

--- batch-server/src/main/kotlin/com/climacast/batch_server/model/repository/WeatherRepository.kt
+++ batch-server/src/main/kotlin/com/climacast/batch_server/model/repository/WeatherRepository.kt
@@ -3,10 +3,11 @@ package com.climacast.batch_server.model.repository
 import com.climacast.batch_server.model.entity.DailyWeather
 import com.climacast.batch_server.model.entity.HourlyWeather
 import com.climacast.batch_server.model.entity.Weather
+import com.climacast.batch_server.model.entity.WeatherId
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface WeatherRepository<T: Weather>: JpaRepository<T, Long>, WeatherJdbcRepository
+interface WeatherRepository<T: Weather>: JpaRepository<T, WeatherId>
 
-interface HourlyWeatherRepository: WeatherRepository<HourlyWeather>
+interface HourlyWeatherRepository: WeatherRepository<HourlyWeather>, WeatherJdbcRepository
 
 interface DailyWeatherRepository: WeatherRepository<DailyWeather>
