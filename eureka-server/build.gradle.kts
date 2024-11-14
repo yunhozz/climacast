13143098076b3b265a870361e335e70e5b84cee4
yunhozz <qkrdbsgh1121@naver.com>
2024-11-15 04:03:57 +0900

    [#9] CHORE: config 의존성 추가

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
 	implementation(project(":global"))
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 }
