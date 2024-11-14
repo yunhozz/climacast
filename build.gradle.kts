d7ee981755df9eccd45f7af1863f3b75f14181c3
yunhozz <qkrdbsgh1121@naver.com>
2024-11-14 11:56:39 +0900

    [#5] CHORE: logback 의존성 오류로 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -75,6 +75,7 @@ subprojects {
 	configurations.forEach {
 		it.exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
 		it.exclude(group = "org.apache.logging.log4j", module = "log4j-to-slf4j")
+		it.exclude(group = "ch.qos.logback", module = "logback-classic")
 	}
 
 	tasks.withType<KotlinCompile> {
