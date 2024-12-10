6e20008a29ff01b7a93ad1addae3856a1029f9cd
yunhozz <qkrdbsgh1121@naver.com>
2024-12-10 18:31:12 +0900

    [dev] FIX: subscription service actuator 작동 문제 수정

--- subscription-service/build.gradle.kts
+++ subscription-service/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
 	implementation(project(":global"))
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
