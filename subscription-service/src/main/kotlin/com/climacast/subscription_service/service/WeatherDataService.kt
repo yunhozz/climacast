201cebb2fd4dd1f16fe5bf034f58c8e7ee3a2dbf
yunhozz <qkrdbsgh1121@naver.com>
2024-12-16 20:29:53 +0900

    [#21] UPDATE: 날씨 데이터 저장 완료 시 로깅 추가

--- subscription-service/src/main/kotlin/com/climacast/subscription_service/service/WeatherDataService.kt
+++ subscription-service/src/main/kotlin/com/climacast/subscription_service/service/WeatherDataService.kt
@@ -1,43 +1,30 @@
 package com.climacast.subscription_service.service
 
-import com.climacast.global.enums.WeatherType
-import com.climacast.subscription_service.dto.WeatherQueryDTO
 import com.climacast.subscription_service.model.document.ForecastWeather
 import com.climacast.subscription_service.model.document.HistoryWeather
-import com.climacast.subscription_service.model.document.WeatherDocument
 import com.climacast.subscription_service.model.repository.ForecastWeatherSearchRepository
 import com.climacast.subscription_service.model.repository.HistoryWeatherSearchRepository
-import com.climacast.subscription_service.service.handler.AbstractWeatherDataHandler
+import com.climacast.subscription_service.service.handler.data.AbstractWeatherDataHandler
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 
 @Service
-class WeatherSearchService(
+class WeatherDataService(
     private val forecastWeatherSearchRepository: ForecastWeatherSearchRepository,
     private val historyWeatherSearchRepository: HistoryWeatherSearchRepository
-): AbstractWeatherDataHandler() {
+) : AbstractWeatherDataHandler() {
+
+    // TODO: 날씨 예보 데이터 입력 시 중복 제거
 
     override fun saveForecastWeathers(forecastWeathers: List<ForecastWeather>): Flux<ForecastWeather> =
         forecastWeatherSearchRepository.saveAll(forecastWeathers)
+            .doOnComplete { log.info("Forecast Weathers Saved") }
             .doOnError { log.error(it.localizedMessage, it) }
             .onErrorResume { Flux.empty() }
 
     override fun saveHistoryWeathers(historyWeathers: List<HistoryWeather>): Flux<HistoryWeather> =
         historyWeatherSearchRepository.saveAll(historyWeathers)
+            .doOnComplete { log.info("History Weathers Saved") }
             .doOnError { log.error(it.localizedMessage, it) }
             .onErrorResume { Flux.empty() }
-
-    fun lookupWeathers(query: WeatherQueryDTO): List<WeatherDocument>? =
-        when (query.type) {
-            WeatherType.FORECAST -> {
-                forecastWeatherSearchRepository.findAll()
-                    .collectList()
-                    .block()
-            }
-            WeatherType.HISTORY -> {
-                historyWeatherSearchRepository.findAll()
-                    .collectList()
-                    .block()
-            }
-        }
 }
