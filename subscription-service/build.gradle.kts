fed88950f871138256798cc320938b15d18dbe0d
yunhozz <qkrdbsgh1121@naver.com>
2024-12-18 21:19:28 +0900

    [#21] CHORE: coroutine 및 slack api 의존성 추가

--- subscription-service/build.gradle.kts
+++ subscription-service/build.gradle.kts
@@ -1,11 +1,14 @@
 dependencies {
 	implementation(project(":global"))
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.8.1")
 	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.redisson:redisson-spring-boot-starter:3.27.0")
+	implementation("com.slack.api:slack-api-client:1.44.2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
