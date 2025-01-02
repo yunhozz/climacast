a1f381a3f5245e5ccb2dd93d289d0ef24cc6331b
yunhozz <qkrdbsgh1121@naver.com>
2025-01-02 19:44:46 +0900

    [#21] FIX: 재시도 원인이 명확하지 않은 문제 수정

--- batch-server/src/main/kotlin/com/climacast/batch_server/config/BatchConfig.kt
+++ batch-server/src/main/kotlin/com/climacast/batch_server/config/BatchConfig.kt
@@ -82,7 +82,6 @@ class BatchConfig(
             .chunk<Region, Region>(chunkSize.toInt(), batchTransactionManager)
             .reader(regionInfoReader())
             .writer(historicalWeatherOpenApiCallWriter(""))
-            .faultTolerant()
             .build()
 
     @Bean
@@ -92,7 +91,6 @@ class BatchConfig(
             .chunk<Region, Region>(chunkSize.toInt(), batchTransactionManager)
             .reader(regionInfoReader())
             .writer(forecastWeatherOpenApiCallWriter(""))
-            .faultTolerant()
             .build()
 
     @Bean
@@ -102,9 +100,6 @@ class BatchConfig(
             .chunk<WeatherResponseDTO, WeatherResponseDTO>(chunkSize.toInt(), appTransactionManager)
             .reader(apiResponseReader())
             .writer(weatherDataWriter())
-            .faultTolerant()
-            .retryLimit(STEP_RETRY_COUNT)
-            .retry(Exception::class.java)
             .build()
 
     @Bean
