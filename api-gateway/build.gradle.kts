20cc20fc805d6313c34a2e7bef9b3ae669e4db2a
yunhozz <qkrdbsgh1121@naver.com>
2025-02-12 19:46:16 +0900

    [#28] DELETE: Spring Cloud Eureka 마이크로서비스 제거

--- api-gateway/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-dependencies {
-	implementation(project(":global"))
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.springframework.cloud:spring-cloud-starter-config")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-	testImplementation("io.projectreactor:reactor-test")
-}
--- eureka-server/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-dependencies {
-	implementation(project(":global"))
-	implementation("org.springframework.cloud:spring-cloud-starter-config")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
-}
