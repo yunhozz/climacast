9b2402efa8eb55228f1b0bed17b3367646e2b249
yunhozz <qkrdbsgh1121@naver.com>
2025-03-10 03:34:18 +0900

    UPDATE: ChromeDriver 인스턴스 생성 시 Remote URL 설정

--- subscription-service/src/main/kotlin/com/climacast/subscription_service/service/handler/document/DocumentVisualizeHandlerImpl.kt
+++ subscription-service/src/main/kotlin/com/climacast/subscription_service/service/handler/document/DocumentVisualizeHandlerImpl.kt
@@ -6,14 +6,15 @@ import com.climacast.subscription_service.model.document.WeatherDocument
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.WebDriverException
-import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.RemoteWebDriver
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
 import java.io.File
 import java.io.IOException
+import java.net.URI
 import java.nio.file.Files
 import java.util.Locale
 import java.util.concurrent.CompletableFuture
@@ -27,6 +28,7 @@ class DocumentVisualizeHandlerImpl(
         const val FORECAST_WEATHER_TEMPLATE = "forecast_weather_template"
         const val HISTORY_WEATHER_TEMPLATE = "history_weather_template"
         const val WEATHER_IMAGE_LOCAL_DIR = "/Users/yunho/Desktop/project/climacast/subscription-service/src/main/resources/image/"
+        const val SELENIUM_CHROME_REMOTE_URL = "http://selenium-chrome:4444"
     }
 
     @Async
@@ -34,35 +36,35 @@ class DocumentVisualizeHandlerImpl(
         region: String,
         document: WeatherDocument,
         type: WeatherType
-    ): CompletableFuture<WeatherDatum> {
-        val datum = WeatherDatum(region, convertDocumentToHtml(document, type))
-        return CompletableFuture.completedFuture(datum)
-    }
-
+    ): CompletableFuture<WeatherDatum> = CompletableFuture.completedFuture(
+        WeatherDatum(region, convertDocumentToHtml(document, type))
+    )
 
     @Async
     override fun convertDocumentToImageAsync(
         region: String,
         document: WeatherDocument,
         type: WeatherType
-    ): CompletableFuture<WeatherDatum> {
-        val datum = WeatherDatum(region, convertDocumentToImage(document, type))
-        return CompletableFuture.completedFuture(datum)
-    }
+    ): CompletableFuture<WeatherDatum> = CompletableFuture.completedFuture(
+        WeatherDatum(region, convertDocumentToImage(document, type))
+    )
 
-    private fun convertDocumentToHtml(document: WeatherDocument, type: WeatherType): String {
-        val context = Context(Locale.getDefault(), mapOf("weatherData" to document))
-        return templateEngine.process(when(type) {
-            WeatherType.FORECAST -> FORECAST_WEATHER_TEMPLATE
-            WeatherType.HISTORY -> HISTORY_WEATHER_TEMPLATE
-        }, context)
-    }
+    private fun convertDocumentToHtml(document: WeatherDocument, type: WeatherType): String =
+        templateEngine.process(
+            when(type) {
+                WeatherType.FORECAST -> FORECAST_WEATHER_TEMPLATE
+                WeatherType.HISTORY -> HISTORY_WEATHER_TEMPLATE
+            }, Context(Locale.getDefault(), mapOf("weatherData" to document))
+        )
 
     private fun convertDocumentToImage(document: WeatherDocument, type: WeatherType): File {
         val html = convertDocumentToHtml(document, type)
-        val chromeDriver = ChromeDriver(ChromeOptions().apply {
-            addArguments("--headless", "--disable-gpu")
-        })
+        val chromeDriver = RemoteWebDriver(
+            URI(SELENIUM_CHROME_REMOTE_URL).toURL(),
+            ChromeOptions().apply {
+                addArguments("--no-sandbox", "--headless", "--disable-gpu")
+            }
+        )
 
         try {
             val currentTime = System.nanoTime()
@@ -83,6 +85,7 @@ class DocumentVisualizeHandlerImpl(
                 is WebDriverException -> throw IllegalArgumentException("Fail to upload images to Chrome: ${e.localizedMessage}", e)
                 else -> throw IllegalArgumentException(e.localizedMessage, e)
             }
+
         } finally {
             chromeDriver.quit()
         }
