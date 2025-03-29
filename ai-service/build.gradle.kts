0a8413233b5e7a69160f8eb6ea0c5374e72b0a9a
yunhozz <qkrdbsgh1121@naver.com>
2025-03-29 15:24:23 +0900

    [#30] CHORE: Ollama 의존성 변경

--- ai-service/build.gradle.kts
+++ ai-service/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("org.springframework.ai:spring-ai-ollama-spring-boot-starter")
+	implementation("org.springframework.ai:spring-ai-starter-model-ollama")
 	implementation(platform("org.springframework.ai:spring-ai-bom:1.0.0-SNAPSHOT"))
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.redisson:redisson-spring-boot-starter:3.27.0")
