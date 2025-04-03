806de63b8c8435df7017ef7f67f8d0de6f3f6188
yunhozz <qkrdbsgh1121@naver.com>
2025-04-03 16:37:24 +0900

    [#30] REFACTOR: 토픽 로깅 부분 개선

--- subscription-service/src/main/kotlin/com/climacast/subscription_service/infra/kafka/WeatherDataListener.kt
+++ subscription-service/src/main/kotlin/com/climacast/subscription_service/infra/kafka/WeatherDataListener.kt
@@ -7,7 +7,8 @@ import com.climacast.subscription_service.infra.document.save.DocumentSaver
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.kafka.support.KafkaHeaders
-import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.MessageHeaders
+import org.springframework.messaging.handler.annotation.Headers
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 
@@ -15,47 +16,56 @@ import org.springframework.stereotype.Component
 class WeatherDataListener(
     private val documentSaver: DocumentSaver
 ) {
-    private val log = logger()
-
     @KafkaListener(topics = [KafkaTopic.WEATHER_FORECAST_TOPIC, KafkaTopic.WEATHER_HISTORY_TOPIC])
     fun receiveWeatherDataTopics(
+        @Headers headers: MessageHeaders,
         @Payload message: KafkaMessage,
-        @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String,
-        @Header(KafkaHeaders.RECEIVED_KEY, required = false) key: String?,
-        @Header(KafkaHeaders.RECEIVED_PARTITION) partition: Int,
-        @Header(KafkaHeaders.OFFSET) offset: Long,
         ack: Acknowledgment
     ) {
-        log.info(
-            """
-            [Weather Data Received] 
-            Topic: $topic
-            Partition: $partition
-            Offset: $offset
-            Key: $key
-            """.trimIndent()
-        )
+        logTopic(headers)
         documentSaver.saveWeathersByMessageType(message)
         ack.acknowledge()
     }
 
-    @KafkaListener(topicPattern = ".*\\.DLT")
-    fun receiveDeadLetterTopics(
-        @Payload dlt: KafkaMessage,
-        @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String,
-        @Header(KafkaHeaders.RECEIVED_KEY, required = false) key: String?,
-        @Header(KafkaHeaders.RECEIVED_PARTITION) partition: Int,
-        @Header(KafkaHeaders.OFFSET) offset: Long
+    @KafkaListener(topics = [KafkaTopic.WEATHER_QUERY_REQUEST_TOPIC])
+    fun receiveWeatherQueryRequestTopic(
+        @Headers headers: MessageHeaders,
+        @Payload message: KafkaMessage,
+        ack: Acknowledgment
     ) {
-        log.error(
-            """
-            [Dead Letter Received] 
-            Topic: $topic
-            Partition: $partition
-            Offset: $offset
-            Key: $key
-            Message: $dlt
-            """.trimIndent()
-        )
+        logTopic(headers)
+        // TODO
+        ack.acknowledge()
+    }
+
+    @KafkaListener(topicPattern = ".*\\.DLT")
+    fun receiveDeadLetterTopics(@Headers headers: MessageHeaders) {
+        logDeadLetterTopic(headers)
+    }
+
+    companion object {
+        private val logger = logger()
+
+        fun logTopic(headers: MessageHeaders) {
+            logger.info(buildString {
+                append("\n[Topic Received]").appendLine()
+                append("Topic: ").appendLine(headers[KafkaHeaders.RECEIVED_TOPIC])
+                append("Partition: ").appendLine(headers[KafkaHeaders.RECEIVED_PARTITION])
+                append("Offset: ").appendLine(headers[KafkaHeaders.ORIGINAL_OFFSET])
+                append("Key: ").appendLine(headers[KafkaHeaders.RECEIVED_KEY])
+            })
+        }
+
+        fun logDeadLetterTopic(headers: MessageHeaders) {
+            logger.error(buildString {
+                append("\n[DLT Received]").appendLine()
+                append("Topic: ").appendLine(headers[KafkaHeaders.DLT_ORIGINAL_TOPIC])
+                append("Partition: ").appendLine(headers[KafkaHeaders.DLT_ORIGINAL_PARTITION])
+                append("Offset: ").appendLine(headers[KafkaHeaders.DLT_ORIGINAL_OFFSET])
+                append("Timestamp: ").appendLine(headers[KafkaHeaders.DLT_ORIGINAL_TIMESTAMP])
+                append("Exception Message: ").appendLine(headers[KafkaHeaders.DLT_EXCEPTION_MESSAGE])
+                append("Raw Data: ").appendLine(headers[KafkaHeaders.RAW_DATA])
+            })
+        }
     }
 }
