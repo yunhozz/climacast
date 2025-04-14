0ed635e6abad2ee312b467bc733215377b87dd62
yunhozz <qkrdbsgh1121@naver.com>
2025-04-14 16:18:55 +0900

    [#30] STYLE: 불필요한 괄호 삭제

--- subscription-service/src/main/kotlin/com/climacast/subscription_service/common/exception/SubscriptionServiceException.kt
+++ subscription-service/src/main/kotlin/com/climacast/subscription_service/common/exception/SubscriptionServiceException.kt
@@ -6,8 +6,8 @@ sealed class SubscriptionServiceException(val errorCode: ApiResponseCode.ErrorCo
     class SubscriptionNotFoundException : SubscriptionServiceException(ApiResponseCode.ErrorCode.SUBSCRIPTION_NOT_FOUND)
     class SubscriptionAlreadyExistException : SubscriptionServiceException(ApiResponseCode.ErrorCode.SUBSCRIPTION_ALREADY_EXIST)
     class SubscriptionAlreadyCanceledException : SubscriptionServiceException(ApiResponseCode.ErrorCode.SUBSCRIPTION_ALREADY_CANCELED)
-    class WeatherDocumentNotFoundException() : SubscriptionServiceException(ApiResponseCode.ErrorCode.WEATHER_DOCUMENT_NOT_FOUND)
-    class ElasticsearchClientException() : SubscriptionServiceException(ApiResponseCode.ErrorCode.INTERNAL_SERVER_ERROR)
-    class WeatherDataSendFailException() : SubscriptionServiceException(ApiResponseCode.ErrorCode.INTERNAL_SERVER_ERROR)
-    class ChromeWebDriverException() : SubscriptionServiceException(ApiResponseCode.ErrorCode.INTERNAL_SERVER_ERROR)
+    class WeatherDocumentNotFoundException : SubscriptionServiceException(ApiResponseCode.ErrorCode.WEATHER_DOCUMENT_NOT_FOUND)
+    class ElasticsearchClientException : SubscriptionServiceException(ApiResponseCode.ErrorCode.INTERNAL_SERVER_ERROR)
+    class WeatherDataSendFailException : SubscriptionServiceException(ApiResponseCode.ErrorCode.INTERNAL_SERVER_ERROR)
+    class ChromeWebDriverException : SubscriptionServiceException(ApiResponseCode.ErrorCode.INTERNAL_SERVER_ERROR)
 }
