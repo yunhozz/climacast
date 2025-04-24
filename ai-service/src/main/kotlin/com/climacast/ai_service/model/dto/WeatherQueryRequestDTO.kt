bec3aa67499cebcc3bbdc83c3612bb2fd3b2587a
yunhozz <qkrdbsgh1121@naver.com>
2025-04-24 17:25:34 +0900

    [#30] UPDATE: validation 보완

--- ai-service/src/main/kotlin/com/climacast/ai_service/model/dto/WeatherQueryRequestDTO.kt
+++ ai-service/src/main/kotlin/com/climacast/ai_service/model/dto/WeatherQueryRequestDTO.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 data class WeatherQueryRequestDTO(
-    @field:NotNull(message = "날씨 종류를 입력해주세요.")
+    @field:NotBlank(message = "날씨 종류를 입력해주세요.")
     val weatherType: String,
 
     @field:NotBlank(message = "상위 지역을 입력해주세요.")
@@ -14,13 +14,16 @@ data class WeatherQueryRequestDTO(
     @field:NotBlank(message = "하위 지역을 입력해주세요.")
     val childRegion: String,
 
-    val isWholeTime: Boolean,
+    @field:NotNull(message = "전체 날짜에 대한 쿼리 여부를 선택해주세요.")
+    val isWholeTime: Boolean?,
+
     val startTime: String?,
     val endTime: String?,
     val message: String?
 ) {
-    @AssertTrue(message = "시작 및 종료 시간대를 입력해주세요.")
-    fun isTimeNotNull(): Boolean =
-        if (!isWholeTime) startTime != null && endTime != null
-        else true
+    @AssertTrue(message = "시작 및 종료 시간대를 다시 한번 확인해주세요.")
+    fun isValidAboutTimes(): Boolean = isWholeTime?.let {
+        if (it) startTime.isNullOrBlank() && endTime.isNullOrBlank()
+        else !startTime.isNullOrBlank() && !endTime.isNullOrBlank()
+    } ?: true
 }
